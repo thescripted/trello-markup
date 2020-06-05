f44a9083e26d8c3fdb770d3ee9f340b2cfbbb67d
Benjamin Kinga <somethingscripted@gmail.com>
2020-06-05 17:07:53 -0500

    added component folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Card from "Components/Card";
 
 function App() {
   return (
     <div className="App">
-     Hello, Ben!
+      <Card />
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className="card-component">
+      This is a Card.
+    </div>
+  );
+};
+
+export default Card
