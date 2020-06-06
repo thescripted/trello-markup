fcee8e02d70da247338c26006fff39d832ee12ce
Benjamin Kinga <somethingscripted@gmail.com>
2020-06-06 15:07:57 -0500

    created list component

---
+++ src/Components/Card.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className="list-wrapper">
+      <div className="card-list">
+        <div className="header-list">
+          <textarea className="header-list-name" spellCheck="false">
+            In Progress
+          </textarea>
+        </div>
+        <div className="card-component f14 lh20">
+          <div className="card-title">
+            Hello, World!
+          </div>
+        </div>
+
+        <div className="card-component f14 lh20">
+          <div className="card-title">
+            Hello, World!
+          </div>
+        </div>
+
+        <div className="card-component f14 lh20">
+          <div className="card-title">
+            Hello, World!
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
