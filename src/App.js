5bafab4349f9cda52b2ef195d6cfc00552927b70
Benjamin Kinga <somethingscripted@gmail.com>
2020-06-07 00:13:21 -0500

    Added background color & formatted header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import CardListContainer from "./Components/CardListContainer";
-
+import Header from "./Components/Header";
 function App() {
   return (
     <div className="App">
+      <Header />
       <CardListContainer />
     </div>
   );
---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="left-header-container">
+        <button className="header-home-button">Home</button>
+        <button className="header-boards-button">Boards</button>
+        <div className="header-search-field">Search Will Be Here...(Coming Soon)</div>
+      </div>
+      <a href="/" className="header-main-logo f20">Logo</a>
+      <div className="right-header-container">
+        <button className="header-add-button">Add</button>
+        <button className="header-info-button">Info</button>
+        <div className="header-user-initial-field">
+          <span style={{height: "32px", width: "32px", lineHeight: "32px", fontSize: "16px"}}>BK</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
