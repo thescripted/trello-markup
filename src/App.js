1a0d798d93fef894ae57bb6d011b0de19fa5316f
Benjamin Kinga <somethingscripted@gmail.com>
2020-06-07 10:24:32 -0500

    minor fixing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CardListContainer from "./Components/CardListContainer";
 import Header from "./Components/Header";
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Header />
--- src/Components/CardListContainer.js
+++ src/Components/CardListContainer.js
@@ -4,6 +4,15 @@ import CardList from './CardList';
 const CardListContainer = () => {
   return (
     <div className="cardlist-canvas">
+      <div className="cardlist-header">
+        <div className="menu-left-header">
+          <div className="title-container f18 lh20">
+            Reactive Schedule Builder
+          </div>
+          <button className="star-icon-button">Star</button>
+        </div>
+        <button className="menu-right-header">Show Menu</button>
+      </div>
       <div className="list-container main-container-scroll">
         <CardList />
         <CardList />
